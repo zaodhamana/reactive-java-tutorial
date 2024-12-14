47091bb5ab2f7f4d072b4b751a96abf121eed6db
zaodhamana <muhorondungu@zaodhamana.com>
2024-12-15 02:37:47 +0300

    file serialization example - Person.java

---
+++ src/main/java/io/javabrains/reactiveworkshop/Person.java
@@ -0,0 +1,36 @@
+package io.javabrains.reactiveworkshop;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public record Person(String name, int age) implements Serializable {
+
+	public static void main(String[] args) {
+	try {
+		Person s = new Person("Muhoro", 1);
+
+		
+		
+		File obj = new File("file1.txt");
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(obj));
+		out.writeObject(s);
+		out.close();
+		
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(obj));
+		Person inperson = null;
+		inperson = (Person)in.readObject();
+		System.out.println(inperson.name+ " " + inperson.age);
+		in.close();
+	}
+		
+	catch(Exception e) 
+	{
+		System.out.println(" " + e.getMessage());
+	}
+	}
+}
